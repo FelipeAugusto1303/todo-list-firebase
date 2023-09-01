9ab118095181712bd12347deda3cc11fdda02a18
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-09-01 01:47:02 -0400

    feat: chakra ui initial configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 import './index.css'
+import ListPage from './pages/ListPage/index.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/todo-list-firebase/',
-    element: <App />,
+    element: <ListPage />,
   },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
 )
