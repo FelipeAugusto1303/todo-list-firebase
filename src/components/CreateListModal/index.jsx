930042d9d4dced33a2abc88dd1785713e2a8cbf6
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-09-01 09:08:34 -0400

    feat:create list modal

---
+++ src/components/CreateListModal/index.jsx
@@ -0,0 +1,48 @@
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react'
+import React, { useRef, useState } from 'react'
+
+// import { Container } from './styles';
+
+function CreateListModal({ isOpen, onClose, handleCreate }) {
+  const initialRef = useRef(null)
+  const [name, setName] = useState('')
+
+  return (
+    <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Crie uma nova lista</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={2}>
+          <Input
+            size='md'
+            placeholder='Digite o nome da lista'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme='teal' mr={3} onClick={() => handleCreate(name)}>
+            Criar
+          </Button>
+          <Button onClick={onClose} mr={3}>
+            Cancelar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default CreateListModal
--- src/pages/ListPage/index.jsx
+++ src/pages/ListPage/index.jsx
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { Flex, Box, Heading, Stack, Input, Button, IconButton } from '@chakra-ui/react'
+import {
+  Flex,
+  Box,
+  Heading,
+  Stack,
+  Input,
+  Button,
+  IconButton,
+  InputGroup,
+  InputLeftElement,
+  Divider,
+  useDisclosure,
+} from '@chakra-ui/react'
 import { createList, findAllLists } from '../../services/firebaseService'
 import { Timestamp, onSnapshot } from 'firebase/firestore'
-import { AddIcon } from '@chakra-ui/icons'
+import { AddIcon, SearchIcon } from '@chakra-ui/icons'
+import CreateListModal from '../../components/CreateListModal'
 
 function ListPage() {
   const [list, setList] = useState(null)
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   console.log('listas encontradas: ', list)
 
@@ -21,56 +35,71 @@ function ListPage() {
     })
   }, [])
 
-  const handleCreateList = () => {
-    console.log('estou aqui')
+  const handleCreateList = (name) => {
     const body = {
-      name: 'List teste',
+      name: name,
       createdBy: 'felipe',
       users: ['felipe'],
       createdAt: Timestamp.now(),
     }
     createList(body)
+    onClose()
   }
 
   return (
-    <Flex align='center' justify='flex-start' direction='column'>
-      <Heading as='h2' size='2xl'>
-        Minhas listas de tarefas
-      </Heading>
-      {list === null || list.length === 0 ? (
-        <Box
-          h='500px'
-          display='flex'
-          flexDirection='column'
-          align='center'
-          justifyContent='center'
-          marginTop='80px'
-          marginBottom='50px'
-          gap='5px'
-        >
-          <Heading as='h3' size='lg'>
-            Parece que você não está em nenhuma lista
-          </Heading>
-          <Heading as='h6' size='sm'>
-            Clique no botão abaixo e crie sua lista
-          </Heading>
-          <IconButton colorScheme='teal' icon={<AddIcon />} onClick={() => handleCreateList()} />
-        </Box>
-      ) : (
-        <Box
-          w='100%'
-          display='flex'
-          direction='row'
-          align='center'
-          justifyContent='flex-start'
-          marginTop='80px'
-          marginBottom='50px'
-        >
-          <Input size='lg' />
-          <Button colorScheme='teal'>Criar Lista</Button>
-        </Box>
-      )}
-    </Flex>
+    <>
+      <Flex align='center' justify='flex-start' direction='column'>
+        <Heading as='h2' size='2xl'>
+          FireTask
+        </Heading>
+        {list === null || list.length === 0 ? (
+          <Box
+            h='500px'
+            display='flex'
+            flexDirection='column'
+            align='center'
+            justifyContent='center'
+            marginTop='80px'
+            marginBottom='50px'
+            gap='5px'
+          >
+            <Heading as='h3' size='lg'>
+              Parece que você não está em nenhuma lista
+            </Heading>
+            <Heading as='h6' size='sm'>
+              Clique no botão abaixo e crie sua lista
+            </Heading>
+            <IconButton colorScheme='teal' icon={<AddIcon />} onClick={() => handleCreateList()} />
+          </Box>
+        ) : (
+          <>
+            <Box
+              w='100%'
+              display='flex'
+              flexDirection='column'
+              align='center'
+              justifyContent='flex-start'
+              marginTop='80px'
+              marginBottom='50px'
+              gap='20px'
+            >
+              <InputGroup>
+                <InputLeftElement pointerEvents='none'>
+                  <SearchIcon color='gray.300' fontSize='20px' />
+                </InputLeftElement>
+                <Input size='md' placeholder='Busque uma lista' />
+              </InputGroup>
+              <Button leftIcon={<AddIcon />} colorScheme='teal' variant='outline' onClick={onOpen}>
+                Crie uma nova Lista
+              </Button>
+              <Divider orientation='horizontal' />
+            </Box>
+            {/* elemento de lista de elementos */}
+          </>
+        )}
+      </Flex>
+      <CreateListModal isOpen={isOpen} onClose={onClose} handleCreate={handleCreateList} />
+    </>
   )
 }
 
