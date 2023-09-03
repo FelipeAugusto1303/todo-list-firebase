426ad571e569f5dff4b23fbdb428764a723359a6
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-09-03 09:05:57 -0400

    feat: skeleton for task page

--- src/components/TaskSkeleton/index.jsx
+++ src/components/TaskSkeleton/index.jsx
@@ -1,15 +1,29 @@
-import { Skeleton, SkeletonCircle, SkeletonText, Stack } from '@chakra-ui/react'
+import { Flex, Skeleton, SkeletonCircle, SkeletonText, Stack } from '@chakra-ui/react'
 import React from 'react'
 
 // import { Container } from './styles';
 
 function TaskSkeleton() {
   return (
-    <Stack w='100%' mt='40px'>
-      <Skeleton height='20px' />
-      <Skeleton height='20px' />
-      <SkeletonCircle size='10' />
-      <SkeletonText mt='4' noOfLines={5} spacing='4' skeletonHeight='2' />
+    <Stack w='100%' mt='80px'>
+      <Flex w='100%' flexDirection='row' alignItems='center' gap='10px'>
+        <Skeleton height='40px' w='90%' />
+        <Skeleton height='40px' w='10%' />
+      </Flex>
+      <Flex w='100%' flexDirection='row' alignItems='center' gap='10px'>
+        <Skeleton height='20px' w='10%' />
+        <SkeletonCircle size='10' />
+        <SkeletonCircle size='10' />
+        <SkeletonCircle size='10' />
+        <SkeletonCircle size='10' />
+      </Flex>
+      <Skeleton height='40px' mt='40px' />
+      <Skeleton height='40px' />
+      <Skeleton height='40px' mt='40px' />
+      <Skeleton height='40px' />
+      <Skeleton height='40px' />
+      <Skeleton height='40px' />
+      <Skeleton height='40px' />
     </Stack>
   )
 }
